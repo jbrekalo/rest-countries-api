a435277343e45386b32a8acf851d8c4506b35897
jbrekalo <josip.brekalo@hotmail.com>
2023-09-07 14:27:22 +0200

    Fetching country details

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const countriesMain = document.querySelector(".countries__main");
 const detailsMain = document.querySelector(".details__main");
 const countriesContainer = document.querySelector(".countries__items");
 const detailsContainer = document.querySelector(".details__container");
-// const countries = document.querySelectorAll(".country");
 const darkmodeToggle = document.querySelector(".header__button");
 const darkmodeIcon = document.querySelector(".header__button-icon");
 const filterContainer = document.querySelector(".countries__filter");
 const filterButton = document.querySelector(".countries__filter-button");
 const filterDropdown = document.querySelector(".countries__filter-dropdown");
+let countries;
+let selectedCountry;
 
 darkmodeToggle.addEventListener("click", (e) => {
   e.preventDefault();
@@ -30,16 +31,16 @@ filterContainer.addEventListener("click", function (e) {
 
 countriesContainer.addEventListener("click", function (e) {
   e.preventDefault();
-  const countries = document.querySelectorAll(".country");
-  const clicked = e.target.closest(".country");
+  const selectedCountry = e.target.closest(".country").dataset.countryName;
 
-  if (!clicked) return;
+  if (!selectedCountry) return;
 
-  countriesMain.classList.add("hidden");
+  getCountryDetails(selectedCountry);
 
-  //   countries.forEach((c) => c.classList.add("hidden"));
+  countriesMain.classList.toggle("hidden");
+  detailsMain.classList.toggle("hidden");
 
-  console.log(clicked);
+  console.log(selectedCountry);
 });
 
 const getCountries = async function () {
@@ -50,13 +51,37 @@ const getCountries = async function () {
   data.forEach((country) => {
     renderCountries(country);
   });
+
+  // if (data) {
+  //   countries = countriesContainer.querySelectorAll(".country");
+  //   moreDetails(countries);
+  // }
 };
 
 getCountries();
 
+// function moreDetails(array) {
+//   console.log(array);
+//   array.forEach((item) => {
+//     item.addEventListener("click", () => {
+//       detailCountry = item.dataset.countryName.toLocaleLowerCase().trim();
+//       console.log(detailCountry);
+//     });
+//   });
+// }
+
+const getCountryDetails = async function (selectedCountry) {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/name/${selectedCountry}`
+  );
+  const data = await response.json();
+
+  renderCountryDetails(data[0]);
+};
+
 const renderCountries = function (country) {
   const markup = `
-  <article class="country" href="#${country.name.common}" >
+  <article class="country" data-country-name="${country.name.common}" >
   <img class="country__img" src="${country.flags.png}" />
   <div class="country__data">
     <h3 class="country__name">${country.name.common}</h3>
@@ -70,28 +95,42 @@ const renderCountries = function (country) {
   `;
 
   countriesContainer.insertAdjacentHTML("beforeend", markup);
-
-  // if (country )
 };
 
-const renderCountryDetails = function () {
+const renderCountryDetails = function (country) {
   const markup = `
-  <div class="details__content">
-                <img src="img/flag.png" alt="" class="details__img">
+            <div class="details__content">
+                <img src="${country.flags.png}" alt="" class="details__img">
                 <div>
-                    <h3 class="details__name">Country</h3>
+                    <h3 class="details__name">${country.name.common}</h3>
                     <div class="details__columns">
                         <div class="details__column">
-                            <p class="details__row"><strong>Native Name: </strong>City</p>
-                            <p class="details__row"><strong>Population: </strong>1,000,000</p>
-                            <p class="details__row"><strong>Region: </strong>Europe</p>
-                            <p class="details__row"><strong>Sub Region: </strong>City</p>
-                            <p class="details__row"><strong>Capital: </strong>City</p>
+                            <p class="details__row"><strong>Native Name: </strong>${
+                              country.name.nativeName
+                            }</p>
+                            <p class="details__row"><strong>Population: </strong>${new Intl.NumberFormat(
+                              "de-DE"
+                            ).format(+country.population)}</p>
+                            <p class="details__row"><strong>Region: </strong>${
+                              country.region
+                            }</p>
+                            <p class="details__row"><strong>Sub Region: </strong>${
+                              country.subregion
+                            }</p>
+                            <p class="details__row"><strong>Capital: </strong>${
+                              country.capital
+                            }</p>
                         </div>
                         <div class="details__column">
-                            <p class="details__row"><strong>Top Level Domain: </strong>1,000,000</p>
-                            <p class="details__row"><strong>Currencies: </strong>Europe</p>
-                            <p class="details__row"><strong>Languages: </strong>City</p>
+                            <p class="details__row"><strong>Top Level Domain: </strong>${
+                              country.tld
+                            }</p>
+                            <p class="details__row"><strong>Currencies: </strong>${
+                              country.currencies
+                            }</p>
+                            <p class="details__row"><strong>Languages: </strong>${
+                              country.languages
+                            }</p>
                         </div>
                     </div>
                     <div class="details__border-container">
