6355c73cb478dcc1984b3c96dcbd2fdb26d9b18b
jbrekalo <josip.brekalo@hotmail.com>
2023-09-07 23:20:15 +0200

    Rendering country details

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const countriesMain = document.querySelector(".countries__main");
 const detailsMain = document.querySelector(".details__main");
 const countriesContainer = document.querySelector(".countries__items");
 const detailsContainer = document.querySelector(".details__container");
+const backButton = document.querySelector(".details__back-button");
 const darkmodeToggle = document.querySelector(".header__button");
-const darkmodeIcon = document.querySelector(".header__button-icon");
 const filterContainer = document.querySelector(".countries__filter");
 const filterButton = document.querySelector(".countries__filter-button");
 const filterDropdown = document.querySelector(".countries__filter-dropdown");
@@ -30,7 +30,7 @@ filterContainer.addEventListener("click", function (e) {
 });
 
 countriesContainer.addEventListener("click", function (e) {
-  e.preventDefault();
+  // e.preventDefault();
   const selectedCountry = e.target.closest(".country").dataset.countryName;
 
   if (!selectedCountry) return;
@@ -43,6 +43,16 @@ countriesContainer.addEventListener("click", function (e) {
   console.log(selectedCountry);
 });
 
+backButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const detailsContent = document.querySelector(".details__content");
+
+  detailsContent.parentNode.removeChild(detailsContent);
+  countriesMain.classList.remove("hidden");
+  detailsMain.classList.add("hidden");
+});
+
 const getCountries = async function () {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
@@ -51,24 +61,9 @@ const getCountries = async function () {
   data.forEach((country) => {
     renderCountries(country);
   });
-
-  // if (data) {
-  //   countries = countriesContainer.querySelectorAll(".country");
-  //   moreDetails(countries);
-  // }
 };
 
 getCountries();
-
-// function moreDetails(array) {
-//   console.log(array);
-//   array.forEach((item) => {
-//     item.addEventListener("click", () => {
-//       detailCountry = item.dataset.countryName.toLocaleLowerCase().trim();
-//       console.log(detailCountry);
-//     });
-//   });
-// }
 
 const getCountryDetails = async function (selectedCountry) {
   const response = await fetch(
@@ -76,68 +71,79 @@ const getCountryDetails = async function (selectedCountry) {
   );
   const data = await response.json();
 
+  console.log(data[0]);
   renderCountryDetails(data[0]);
 };
 
 const renderCountries = function (country) {
   const markup = `
   <article class="country" data-country-name="${country.name.common}" >
-  <img class="country__img" src="${country.flags.png}" />
-  <div class="country__data">
-    <h3 class="country__name">${country.name.common}</h3>
-    <p class="country__row"><strong>Population: </strong>${new Intl.NumberFormat(
-      "de-DE"
-    ).format(+country.population)}</p>
-    <p class="country__row"><strong>Region: </strong>${country.region}</p>
-    <p class="country__row"><strong>Capital: </strong>${country.capital}</p>
-  </div>
-</article>
+    <img class="country__img" src="${country.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${country.name.common}</h3>
+      <p class="country__row"><strong>Population: </strong>${new Intl.NumberFormat(
+        "de-DE"
+      ).format(+country.population)}</p>
+      <p class="country__row"><strong>Region: </strong>${country.region}</p>
+      <p class="country__row"><strong>Capital: </strong>${country.capital}</p>
+    </div>
+  </article>
   `;
 
   countriesContainer.insertAdjacentHTML("beforeend", markup);
 };
 
 const renderCountryDetails = function (country) {
+  const borderContainer = document.querySelector("details__border-container");
+  console.log(country);
   const markup = `
-            <div class="details__content">
-                <img src="${country.flags.png}" alt="" class="details__img">
-                <div>
-                    <h3 class="details__name">${country.name.common}</h3>
-                    <div class="details__columns">
-                        <div class="details__column">
-                            <p class="details__row"><strong>Native Name: </strong>${
-                              country.name.nativeName
-                            }</p>
-                            <p class="details__row"><strong>Population: </strong>${new Intl.NumberFormat(
-                              "de-DE"
-                            ).format(+country.population)}</p>
-                            <p class="details__row"><strong>Region: </strong>${
-                              country.region
-                            }</p>
-                            <p class="details__row"><strong>Sub Region: </strong>${
-                              country.subregion
-                            }</p>
-                            <p class="details__row"><strong>Capital: </strong>${
-                              country.capital
-                            }</p>
-                        </div>
-                        <div class="details__column">
-                            <p class="details__row"><strong>Top Level Domain: </strong>${
-                              country.tld
-                            }</p>
-                            <p class="details__row"><strong>Currencies: </strong>${
-                              country.currencies
-                            }</p>
-                            <p class="details__row"><strong>Languages: </strong>${
-                              country.languages
-                            }</p>
-                        </div>
-                    </div>
-                    <div class="details__border-container">
-                        <h3 class="details__border-label">Border Countries:</h3><button class="details__neighbour">France</button>
-                    </div>
-                </div>
-            </div>
+  <div class="details__content">
+      <img src="${country.flags.png}" alt="" class="details__img">
+      <div>
+          <h3 class="details__name">${country.name.common}</h3>
+          <div class="details__columns">
+              <div class="details__column">
+                  <p class="details__row"><strong>Native Name: </strong>${
+                    Object.values(country.name.nativeName)[0].common
+                  }</p>
+                  <p class="details__row"><strong>Population: </strong>${new Intl.NumberFormat(
+                    "de-DE"
+                  ).format(+country.population)}</p>
+                  <p class="details__row"><strong>Region: </strong>${
+                    country.region
+                  }</p>
+                  <p class="details__row"><strong>Sub Region: </strong>${
+                    country.subregion
+                  }</p>
+                  <p class="details__row"><strong>Capital: </strong>${
+                    country.capital
+                  }</p>
+              </div>
+              <div class="details__column">
+                  <p class="details__row"><strong>Top Level Domain: </strong>${
+                    country.tld
+                  }</p>
+                  <p class="details__row"><strong>Currencies: </strong>${
+                    Object.values(country.currencies)[0].name
+                  }</p>
+                  <p class="details__row"><strong>Languages: </strong>${Object.values(
+                    country.languages
+                  )
+                    .toString()
+                    .replaceAll(",", ", ")}</p>
+              </div>
+          </div>
+          <div class="details__border-container">
+              <h3 class="details__border-label">Border Countries:</h3>
+              ${country.borders
+                .map(
+                  (border) =>
+                    `<button class="details__neighbour">${border}</button>`
+                )
+                .join("")}         
+          </div>
+      </div>
+  </div>
   `;
 
   detailsContainer.insertAdjacentHTML("beforeend", markup);
